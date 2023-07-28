4d2b5678ceed7d88dc67a056030912f5986c0564
Easton Huch <easton.huch@gmail.com>
2023-07-28 15:27:25 -0400

    WCLS now accounts for uncertainty in p_r

--- simulation.R
+++ simulation.R
@@ -5,52 +5,162 @@ set.seed(1)
 source("~/Documents/research/mrt-data-integration/generate_data.R")
 source("~/Documents/research/mrt-data-integration/eastons-method.R")
 source("~/Documents/research/mrt-data-integration/walters-method.R")
+source("~/Documents/research/mrt-data-integration/wcls.R")
 require(geepack)
+require(abind)
 
-# Generate data
-n <- 1000
-run_simulation <- function() {
-  dat <- generate_data(dof=1e10, ar_param=1e-10, t_max=2, n_internal=n, n_external=n)
-  c(eastons_method(dat)$beta_r_chi2, walters_method(dat)$beta_r_chi2)
+# True value of beta_r
+beta_r_true <- c(-5, -1, 0.9, 0.3)
+names(beta_r_true) <- c("A - p(A=1 | s)", "[A - p(A=1 | s)] * x1", "[A - p(A=1 | s)] * x1^2", "[A - p(A=1 | s)] * x1^3")
+
+# Function for applying WCLS to data
+apply_wcls <- function(data) {
+  p_s_mod <- glm(a ~ 1, data=data, family=binomial())
+  data$p_s_hat <- predict(p_s_mod, newdata=data, type="response")
+  data$p_s_hat_a <- data$a * data$p_s_hat + (1 - data$a) * (1 - data$p_s_hat)
+  data$w <- data$p_s_hat_a / data$p_h_a
+  data$a_centered <- data$a - data$p_s_hat
+  
+  # TODO: Update this method to account for uncertainty in p_mod
+  model_wcls <- geeglm(
+    y ~ x1 + x2 + x3 + I(a_centered) + I(a_centered * x1) + I(a_centered * x1^2) + + I(a_centered * x1^3),
+    data=data, weights=w, id=user_id
+  )
+  model_summary_wcls <- summary(model_wcls)
+  vcov_wcls <- vcov(model_wcls)
+  wcls_beta_r_idx <- seq(5, 8)
+  beta_r_wcls <- model_wcls$coefficients[wcls_beta_r_idx]
+  se_wcls <- sqrt(diag(vcov_wcls))[wcls_beta_r_idx]
+  covered_wcls <- (
+    (beta_r_true >= beta_r_wcls - 1.96 * se_wcls) & 
+      (beta_r_true <= beta_r_wcls + 1.96 * se_wcls))
+  
+  results <- cbind(
+    estimate=beta_r_wcls,
+    se=se_wcls,
+    covered=covered_wcls
+  )
+  results
 }
 
-n_replications <- 100
-chi2_values <- replicate(n_replications, run_simulation())
+# Function for simulating one data set and applying methods
+simulate_one <- function(n_internal, n_external) {
+  dat <- generate_data(t_max=20, dof=10, n_internal=n_internal, n_external=n_external, ar_param=0.5)
+  
+  # Walter method
+  model_walter <- walters_method(dat)
+  covered_walter <- (
+    (beta_r_true >= model_walter$beta_r - 1.96 * model_walter$se_beta_r) & 
+    (beta_r_true <= model_walter$beta_r + 1.96 * model_walter$se_beta_r))
+  results_walter <- cbind(
+    estimate=model_walter$beta_r,
+    se=model_walter$se_beta_r,
+    covered=covered_walter
+  )
+  
+  # Internal only
+  dat_internal <- dat[dat$is_internal, ]
+  results_internal <- apply_wcls(dat_internal)
+  
+  # Naive pooling
+  results_naive <- apply_wcls(dat)
+  
+  # Bind results together
+  results <- abind(
+    results_walter,
+    results_internal,
+    results_naive,
+    along=3
+  )
+  dimnames(results)[[3]] <- c("P-WCLS", "WCLS, Internal Only", "WCLS, Naive Pooling")
+  results
+}
 
-max_chi2 <- max(chi2_values)
-chi2_breaks <- seq(0, max_chi2+1)
-#chi2_breaks <- seq(0, max(chi2_values[1,])+1)
-hist(chi2_values[1,], breaks=chi2_breaks)
-hist(chi2_values[2,], breaks=chi2_breaks) # This must be wrong
-hist(rchisq(n_replications, 4), breaks=chi2_breaks)
+# Run simulation
+simulate_all <- function(n_internal, n_external, n_replications) {
+  results <- replicate(n_replications, simulate_one(n_internal=n_internal, n_external=n_external))
+  
+  # Process results
+  coverage <- apply(results[,"covered",,], MARGIN=c(1, 2), FUN=mean)
+  avg_estimate <- apply(results[,"estimate",,], MARGIN=c(1, 2), FUN=mean)
+  bias <- avg_estimate - beta_r_true
+  empirical_se <- sqrt(apply(
+    (results[,"estimate",,] - (avg_estimate %x% array(1, dim=c(1, 1, n_replications))))^2,
+    MARGIN=c(1, 2),
+    FUN=mean))
+  empirical_relative_efficiency <- empirical_se / empirical_se[, "P-WCLS"]
+  avg_analytical_se <- apply(results[,"se",,], MARGIN=c(1, 2), FUN=mean)
+  analytical_relative_efficiency <- avg_analytical_se / avg_analytical_se[, "P-WCLS"]
+  mse <- apply((results[,"estimate",,] - beta_r_true)^2, MARGIN=c(1, 2), FUN=mean)
+  rmse <- sqrt(mse)
+  
+  # Create results list
+  list(
+    results=results,
+    coverage=coverage,
+    avg_estimate=avg_estimate,
+    bias=bias,
+    empirical_se=empirical_se,
+    empirical_relative_efficiency=empirical_relative_efficiency,
+    avg_analytical_se=avg_analytical_se,
+    analytical_relative_efficiency=analytical_relative_efficiency,
+    mse=mse,
+    rmse=rmse
+  )
+}
 
-mean(chi2_values[1,])
-mean(chi2_values[2,])
+# Make table
+# Let's do this one parameter at a time
+create_pretty_table <- function(result_list) {
+  n_methods <- ncol(result_list$coverage)
+  n_coefs <- nrow(result_list$coverage)
+  coef_table <- data.frame()
+  
+  coef_table <- cbind(
+    rep(beta_r_true, each=n_methods),
+    c(t(result_list$avg_estimate)),
+    c(t(result_list$empirical_se)),
+    c(t(result_list$empirical_relative_efficiency)),
+    c(t(result_list$avg_analytical_se)),
+    c(t(result_list$analytical_relative_efficiency)),
+    c(t(result_list$rmse)),
+    c(t(result_list$coverage))
+  )
+  coef_table <- round(coef_table, digits=3) %>% as.data.frame()
+  coef_table <- cbind(
+    rep(names(beta_r_true), each=n_methods),
+    rep(colnames(result_list$coverage), times=n_coefs),
+    coef_table)
+  row.names(coef_table) <- NULL
+  colnames(coef_table) <- c(
+    "Coefficient Name",
+    "Method",
+    "True Value",
+    "Avg Estimate",
+    "Empirical Standard Error",
+    "Empirical Relative Efficiency",
+    "Analytical Standard Error",
+    "Analytical Relative Efficiency",
+    "rMSE",
+    "Coverage (95% Nominal)"
+  )
+  coef_table  
+}
 
-median(chi2_values[1,])
-median(chi2_values[2,])
+n_replications <- 400
 
-dat <- generate_data(dof=1e10, ar_param=1e-10, t_max=2, n_internal=1000, n_external=1000)
+# 25 external data points
+results_25 <- simulate_all(100, 25, n_replications)
+View(create_pretty_table(results_25))
 
-dat$p_s <- mean(dat$a)
-dat$a_centered <- dat$a - dat$p_s
-dat$p_s_a <- dat$a * dat$p_s + (1 - dat$a) * (1 - dat$p_s)
-dat$w <- dat$p_s_a / dat$p_h_a
-#wcls_mod <- geeglm(
-#  y ~ x1 + x2 + x3 + I(a_centered) + I(a_centered * x1) + I(a_centered * x2),
-# data=dat,
-# id=user_id
-#)
+# 50 external data points
+results_50 <- simulate_all(100, 50, n_replications)
+View(create_pretty_table(results_50))
 
-eastons_results <- eastons_method(dat)
-walters_results <- walters_method(dat)
-#sqrt(diag(vcov(wcls_mod)))[5:7]
-eastons_results$se_beta_s
-sqrt(diag(walters_results$sandwich))[6:8]
+# 100 external data points
+results_100 <- simulate_all(100, 100, n_replications)
+View(create_pretty_table(results_100))
 
-eastons_results$beta_r
-walters_results$beta_r
-eastons_results$se_beta_r
-walters_results$se_beta_r
-eastons_results$beta_r_z_scores
-walters_results$beta_r_z_scores
+# 200 external data points
+results_200 <- simulate_all(100, 200, n_replications)
+View(create_pretty_table(results_200))
---
+++ wcls.R
@@ -0,0 +1,139 @@
+wcls_sandwich <- function(data, models, beta_h_formula, beta_r_formula) {
+  # Extract some columns
+  y <- data$y
+  p_h_a <- data$p_h_a
+  a <- data$a
+  a_centered <- data$a_centered
+
+  # Construct design matrices
+  X_alpha_r <- model.matrix(formula(models$p_r), data=data)
+  X_beta_h <- model.matrix(beta_h_formula, data=data)
+  X_beta_r <- model.matrix(beta_r_formula, data=data)
+  X_beta_r_raw <- X_beta_r / a_centered
+  X_beta_hr <- cbind(X_beta_h, X_beta_r)
+  
+  # Store dimensions
+  n <- nrow(X_beta_h)
+  d_alpha_r <- ncol(X_alpha_r)
+  d_h <- ncol(X_beta_h)
+  d_r <- ncol(X_beta_r)
+  d_hr <- d_h + d_r
+  d <- d_alpha_r + d_h + d_r
+  
+  # Extract coefficients
+  alpha_r <- coef(models$p_r)
+  beta_hr <- coef(models$wcls)
+  beta_h <- beta_hr[seq(d_h)]
+  beta_r <- beta_hr[seq(d_h+1, d_h+d_r)]
+  
+  # Construct position vectors
+  pos_alpha_r <- seq(d_alpha_r)
+  pos_beta_h <- d_alpha_r + seq(d_h)
+  pos_beta_r <- max(pos_beta_h) + seq(d_r)
+  pos_beta_hr <- c(pos_beta_h, pos_beta_r)
+  
+  # p_r_hat score/hessian
+  scores <- matrix(0, nrow=n, ncol=d)
+  hessian <- matrix(0, nrow=d, ncol=d)
+  p_r_hat <- 1 / (1 + exp(-c(X_alpha_r %*% alpha_r)))  # Assume logit link
+  scores[, pos_alpha_r] <- (a - p_r_hat) * X_alpha_r
+  sd_p_r_hat <- sqrt(p_r_hat * (1-p_r_hat))
+  X_alpha_r_scaled <- sd_p_r_hat * X_alpha_r
+  hessian[pos_alpha_r, pos_alpha_r] <- crossprod(X_alpha_r_scaled)
+  
+  # WCLS scores and Hessian
+  p_r_hat_a <- a*p_r_hat + (1-a)*(1-p_r_hat)
+  w <- p_r_hat_a / p_h_a
+  wcls_h_fitted_values <- c(X_beta_h %*% beta_h)
+  wcls_r_fitted_values <- c(X_beta_r %*% beta_r)
+  wcls_resids <- c(y - wcls_h_fitted_values - wcls_r_fitted_values)
+  wcls_weighted_resids <- w * wcls_resids
+  scores[, pos_beta_h] <- wcls_weighted_resids * X_beta_h
+  scores[, pos_beta_r] <- wcls_weighted_resids * X_beta_r
+  
+  sqrt_w <- sqrt(w)
+  X_beta_hr_scaled <- sqrt_w * X_beta_hr
+  GtWG <- crossprod(X_beta_hr_scaled)
+  hessian[pos_beta_hr, pos_beta_hr] <- GtWG
+
+  p_r_hat_a_deriv <- -(2*a-1) * p_r_hat * (1 - p_r_hat)
+  log_p_r_hat_a_deriv <- p_r_hat_a_deriv / p_r_hat_a
+  p_r_deriv <- -(1-p_r_hat)
+  p_r_X_beta_r <- p_r_hat * X_beta_r_raw
+  hessian[pos_beta_hr, pos_alpha_r] <- 
+    t(X_beta_hr * wcls_weighted_resids) %*% log_p_r_hat_a_deriv +
+    t(cbind(matrix(0, nrow=n, ncol=d_h), -p_r_X_beta_r) * wcls_weighted_resids) %*% p_r_deriv +
+    t(X_beta_hr * (p_r_hat * wcls_r_fitted_values / a_centered * w)) %*% p_r_deriv
+
+  # Assemble sandwich
+  meat <- crossprod(scores)
+  half_sandwich <- solve(hessian, t(chol(meat)))
+  sandwich <- tcrossprod(half_sandwich)
+  list(
+    sandwich=sandwich,
+    bread=hessian,
+    meat=meat
+  )
+}
+
+wcls <- function(data) {
+  beta_r_true <- c(-5, -1, 0.9, 0.3)
+  
+  # Get point estimates
+  # Propensity score model
+  p_r_mod <- glm(a ~ 1, data=data, family=binomial())
+  alpha_r <- coef(p_r_mod)
+  data$p_r_hat <- predict(p_r_mod, newdata=data, type="response")
+  data$a_centered <- data$a - data$p_r_hat
+  data$p_r_hat_a <- data$a * data$p_r_hat + (1 - data$a) * (1 - data$p_r_hat)
+  data$w <- data$p_r_hat_a / data$p_h_a
+  
+  # WCLS
+  beta_h_formula <- y ~ x1 + x2 + x3
+  beta_r_formula <- y ~ 0 + I(a_centered) + I(a_centered * x1) + I(a_centered * x1^2) + I(a_centered * x1^3)
+  beta_r_formula_character <- as.character(update(beta_r_formula, . ~ . + 1))[3]
+  beta_r_formula_symbol <- rlang::parse_expr(beta_r_formula_character)
+  wcls_formula <- update(beta_h_formula, bquote(. ~ . + .(beta_r_formula_symbol)))
+  wcls_mod <- lm(wcls_formula, data=data, weights=w)
+  last_beta_h_idx <- length(attr(terms(beta_h_formula), "term.labels")) + 1
+  beta_h <- coef(wcls_mod)[ seq(last_beta_h_idx)]
+  beta_r <- coef(wcls_mod)[-seq(last_beta_h_idx)]
+  data$wcls_r_causal_effects <- c(model.matrix(beta_r_formula, data=data) %*% beta_r) / data$a_centered
+  d_r <- length(beta_r)
+  
+  # Models list
+  models <- list(
+    p_r=p_r_mod,
+    wcls=wcls_mod
+  )
+  
+  # Total number of parameters
+  vector_estimate <- c(
+    alpha_r,
+    beta_h,
+    beta_r
+  )
+  n_params <- length(vector_estimate)
+  
+  # Standard errors
+  sandwich_list <- wcls_sandwich(data, models, beta_h_formula, beta_r_formula)
+  sandwich <- sandwich_list$sandwich
+  pos_beta_r <- length(alpha_r) + length(beta_h) + seq_along(beta_r)
+  var_beta_r <- sandwich[pos_beta_r, pos_beta_r]
+  se_beta_r <- sqrt(diag(var_beta_r))
+  beta_r_error <- beta_r - beta_r_true
+  beta_r_z_scores <- beta_r_error / se_beta_r
+  beta_r_chi2 <- beta_r_error %*% solve(var_beta_r, beta_r_error)
+  
+  results <- list(
+    beta_r=beta_r,
+    se_beta_r=se_beta_r,
+    var_beta_r=var_beta_r,
+    beta_r_chi2=beta_r_chi2,
+    beta_r_z_scores=beta_r_z_scores,
+    sandwich=sandwich,
+    bread=sandwich_list$bread,
+    meat=sandwich_list$meat
+  )
+  results
+}
