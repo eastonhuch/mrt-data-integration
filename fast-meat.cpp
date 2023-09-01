92c22e634fd955419dc458d68766464567feec49
Easton Huch <easton.huch@gmail.com>
2023-09-01 17:37:24 -0400

    fast-meat is working

--- fast-meat.cpp
+++ fast-meat.cpp
@@ -4,83 +4,68 @@
 //[[Rcpp::depends(RcppArmadillo)]]
 
 //[[Rcpp::export]]
-arma::mat getMeat(arma::mat bread, arma::cube D, arma::mat v, arma::mat r) {
+arma::mat getMeat(arma::mat D, arma::colvec v, arma::colvec r, int t) {
   // Check inputs
-  int P = bread.n_rows;
-  if (bread.n_cols != P) {
-    throw std::invalid_argument("bread must be symmetric");
-  }
-    
-  int K = D.n_rows;
-  int T = D.n_cols;
-  if (D.n_slices != P) {
-    throw std::invalid_argument("Last dimension of D must == bread.n_cols");
+  int nt = D.n_rows;
+  if (nt % t != 0) {
+    throw std::invalid_argument("D.n_rows is not a multiple of t");
   }
   
-  if (v.n_rows != K) {
-    throw std::invalid_argument("First dimension of v must == D.n_slices");
+  if (v.n_rows != nt) {
+    throw std::invalid_argument("D and v (as a column vector) must have the same number of rows");
   }
   
-  if (v.n_cols != T) {
-    throw std::invalid_argument("Second dimension of v must == D.n_rows");
-  }
-  
-  if (r.n_rows != K) {
-    throw std::invalid_argument("First dimension of r must == D.n_slices");
-  }
-  
-  if (r.n_cols != T) {
-    throw std::invalid_argument("Second dimension of r must == D.n_rows");
+  if (r.n_rows != nt) {
+    throw std::invalid_argument("D and r (as a column vector) must have the same number of rows");
   }
   
   // Create meat matrix
-  arma::mat meat = arma::zeros(P, P);
+  int p = D.n_cols;
+  arma::mat meat = arma::zeros(p, p);
   
   // Loop through participants
-  arma::mat D_k;
-  arma::mat Dt_vi_k;
-  arma::rowvec v_k;
-  arma::colvec z;
-  arma::mat H;
-  for (int k = 0; k < K; k++) {
-    D_k = D.row(k);
-    v_k = v.row(k);
-    Dt_vi_k = D_k.t();
-    Dt_vi_k.each_row() /= v_k;
-    H = D_k * bread * Dt_vi_k;
-    z = Dt_vi_k * arma::solve(arma::eye(T, T) - H, r.row(k).t());
-    meat += z * z.t();
+  int idx_upper;
+  arma::mat D_subset;
+  arma::colvec v_subset;
+  arma::colvec r_subset;
+  arma::rowvec z;
+  for (int idx_lower = 0; idx_lower < nt; idx_lower += t) {
+    idx_upper = idx_lower + t - 1;
+    D_subset = D.rows(idx_lower, idx_upper);
+    v_subset = v.rows(idx_lower, idx_upper);
+    r_subset = r.rows(idx_lower, idx_upper);
+    z = (r_subset / v_subset).t() * D_subset;
+    meat += z.t() * z;
   }
 
   return meat;
 }
 
 /*** R
-P_val <- 10
-K_val <- 1000
-T_val <- 20
-x <- matrix(rnorm(P_val^2), nrow=P_val, ncol=P_val)
-bread <- crossprod(x)
-D <- array(rnorm(P_val*K_val*T_val), dim=c(K_val, T_val, P_val))
-v <- matrix(exp(rnorm(K_val * T_val)), nrow=K_val, ncol=T_val)
-r <- matrix(rnorm(K_val * T_val), nrow=K_val, ncol=T_val)
+require(microbenchmark)
+P_val <- 5
+K_val <- 34
+T_val <- 11
+D <- matrix(rnorm(P_val*K_val*T_val), nrow=K_val*T_val, ncol=P_val)
+v <- exp(rnorm(K_val * T_val))
+r <- rnorm(K_val * T_val)
 
-meat_cpp <- getMeat(bread, D, v, r)
+meat_cpp <- getMeat(D, v, r, T_val)
 
-getMeatR <- function(bread, D, v, r) {
+getMeatR <- function(D, v, r, t) {
   meat_R <- matrix(0, nrow=P_val, ncol=P_val)
-  for (k in seq(K_val)) {
-    D_k <- D[k,,]
-    DtVi <- t(D_k / v[k,])
-    H <- D_k %*% bread %*% DtVi
-    z <- DtVi %*% solve(diag(T_val) - H, c(r[k,]))
-    meat_inc <- tcrossprod(z)
+  for (k in seq(0, K_val-1)) {
+    idx_lower <- k * T_val
+    idx_upper <- (k+1) * T_val - 1
+    idx <- (idx_lower:idx_upper) + 1
+    z <- t(r[idx] / v[idx]) %*% D[idx,]
+    meat_inc <- crossprod(z)
     meat_R <- meat_R + meat_inc
   }
   meat_R
 }
-meat_R <- getMeatR(bread, D, v, r)
+meat_R <- getMeatR(D, v, r, T_val)
 
 all(meat_cpp == meat_R)
-microbenchmark("R" = {getMeatR(bread, D, v, r)}, "Rcpp" = {getMeat(bread, D, v, r)}, times=10)
+microbenchmark("R" = {getMeatR(D, v, r, T_val)}, "Rcpp" = {getMeat(D, v, r, T_val)}, times=40)
 */
